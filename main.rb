46d117ea9520aefee9b0bae9f1398b6be9c10b39
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-01-30 15:03:32 +0500

    board commented out

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative './lib/board.rb'
 require_relative './lib/pawn.rb'
 require_relative './lib/rook.rb'
 
-b = Board.new
+# b = Board.new
 
 puts b.in_bounds?([-1, 0])
 puts b.in_bounds?([0, 0])
