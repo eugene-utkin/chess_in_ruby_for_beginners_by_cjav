ed927d10c6c5dac866e2da5d5dd59e5573b58eb2
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-11 20:49:40 +0500

    new board created

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative './lib/board.rb'
 require_relative './lib/pawn.rb'
 require_relative './lib/rook.rb'
 
-# b = Board.new
+b = Board.new
 
 pawn = Pawn.new([0, 0], :black)
 p pawn.to_s
