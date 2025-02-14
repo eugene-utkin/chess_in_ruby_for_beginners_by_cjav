1cece6c72578782881336a1059bb0cf862dc2188
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-14 19:26:29 +0500

    pawn created

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require_relative './lib/rook.rb'
 b = Board.new
 b[[1, 0]] = Pawn.new([0, 0], :black)
 b[[1, 1]] = Pawn.new([0, 0], :black)
+b[[1, 2]] = Pawn.new([0, 0], :black)
 
 pawn = Pawn.new([0, 0], :black)
 p pawn.to_s
