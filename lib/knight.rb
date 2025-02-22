91428e92c2b6c2baf43128f01dd5783dac5b832c
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-02-22 16:32:13 +0500

    pawn coded copied to Knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,3 +1,17 @@
 class Knight
+  attr_reader :color
 
+  def initialize(color)
+    @color = color
+  end
+
+  def to_s
+    color == :black ? "♟" : "♙"
+  end
+
+  def move_dirs
+    [
+      [0, 1]
+    ]
+  end
 end
