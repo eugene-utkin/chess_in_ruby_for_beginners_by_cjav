4333638cf4abc5e7e012903116bbc454b4de0bd8
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-03-24 19:38:56 +0500

    text_board created

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ b[[1, 1]] = Pawn.new([0, 0], :black)
 b[[1, 2]] = Pawn.new([0, 0], :black)
 b[[1, 3]] = Pawn.new([0, 0], :white)
 
-puts b.grid
+text_board = BoardRendererText.new(b)
 
 # pawn = Pawn.new([0, 0], :black)
 # p pawn.to_s
