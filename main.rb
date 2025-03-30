3470f70ad2f84223353ea2a46720b15598150f33
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-03-30 18:45:36 +0500

    requirements sorted

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
+require_relative './lib/bishop.rb'
 require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
-require_relative './lib/pawn.rb'
-require_relative './lib/rook.rb'
-require_relative './lib/knight.rb'
 require_relative './lib/king.rb'
+require_relative './lib/knight.rb'
+require_relative './lib/pawn.rb'
 require_relative './lib/queen.rb'
-require_relative './lib/bishop.rb'
+require_relative './lib/rook.rb'
 
 b = Board.new
 b[[1, 0]] = Pawn.new(:black)
