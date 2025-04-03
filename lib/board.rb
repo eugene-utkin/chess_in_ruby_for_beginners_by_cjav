69854c5130f7b83a91c8400c473a84a7481af1d1
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-04-03 18:21:26 +0500

    pieces required

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,10 @@
+require_relative './bishop.rb'
+require_relative './king.rb'
+require_relative './knight.rb'
+require_relative './pawn.rb'
+require_relative './queen.rb'
+require_relative './rook.rb'
+
 class Board
   attr_reader :grid
 
