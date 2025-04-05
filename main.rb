fe2ae03515fc9f74d429a51b7126f877a6753412
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-04-05 22:20:14 +0500

    start_chess called

--- main.rb
+++ main.rb
@@ -7,11 +7,7 @@ require_relative './lib/pawn.rb'
 require_relative './lib/queen.rb'
 require_relative './lib/rook.rb'
 
-b = Board.new
-b[[1, 0]] = Pawn.new(:black)
-b[[1, 1]] = Pawn.new(:black)
-b[[1, 2]] = Pawn.new(:black)
-b[[1, 3]] = Pawn.new(:white)
+b = Board.start_chess
 
 text_board = BoardRendererText.new(b)
 puts text_board.render
