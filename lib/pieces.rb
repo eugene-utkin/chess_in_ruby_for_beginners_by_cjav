0d3681baef6734279d1d79eac6b3a1ee47ede3e5
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-03 14:55:38 +0500

    require moved

---
+++ lib/pieces.rb
@@ -0,0 +1,8 @@
+require_relative './bishop.rb'
+require_relative './board.rb'
+require_relative './board_renderer_text.rb'
+require_relative './king.rb'
+require_relative './knight.rb'
+require_relative './pawn.rb'
+require_relative './queen.rb'
+require_relative './rook.rb'
