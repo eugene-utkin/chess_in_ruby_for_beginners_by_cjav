f3503c77c698d3642c82a2da898531da87d428e2
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-04 15:51:39 +0500

    pieces required

--- main.rb
+++ main.rb
@@ -1,11 +1,6 @@
-require_relative './lib/bishop.rb'
+require_relative './lib/pieces.rb'
 require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
-require_relative './lib/king.rb'
-require_relative './lib/knight.rb'
-require_relative './lib/pawn.rb'
-require_relative './lib/queen.rb'
-require_relative './lib/rook.rb'
 
 b = Board.new
 b[[0, 0]] = Pawn.new(b, :black)
