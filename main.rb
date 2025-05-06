384eebc2eea1df189d4c171dc6570fec1454be30
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-06 13:41:51 +0500

    board render commented out

--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ piece = b[[0, 0]]
 puts piece.enemy?([0, 1])
 puts piece.enemy?([0, 0])
 
-text_board = BoardRendererText.new(b)
-puts text_board.render
+# text_board = BoardRendererText.new(b)
+# puts text_board.render
 
 # pawn = Pawn.new([0, 0], :black)
 # p pawn.to_s
