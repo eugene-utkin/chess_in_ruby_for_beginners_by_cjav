8408fdb65a27279836b07e7e59317999a330b639
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-05-07 18:35:49 +0500

    rook placed

--- main.rb
+++ main.rb
@@ -3,12 +3,7 @@ require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
 
 b = Board.new
-b[[0, 0]] = Pawn.new(b, :black)
-b[[0, 1]] = Pawn.new(b, :white)
-
-piece = b[[0, 0]]
-puts piece.enemy?([0, 1])
-puts piece.enemy?([0, 0])
+b[[0, 0]] = Rook.new(b, :black)
 
 # text_board = BoardRendererText.new(b)
 # puts text_board.render
