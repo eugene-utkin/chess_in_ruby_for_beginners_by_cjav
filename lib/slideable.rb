066f62efa662c9d5248e0f17a639010b78b2dead
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-06-02 22:16:39 +0500

    available_moves method moved into module

--- lib/slideable.rb
+++ lib/slideable.rb
@@ -1,3 +1,27 @@
 module Slideable
+  def available_moves
+    moves = []
 
+    move_dirs.each do |(dr, dc)|
+      current_r, current_c = location
+
+      loop do
+        current_r += dr
+        current_c += dc
+        loc = [current_r, current_c]
+        break if !board.in_bounds?(loc)
+
+        if board.empty?(loc)
+          moves << loc
+        end
+        if enemy?(loc)
+          moves << loc
+          break
+        end
+      end
+      # until we hit a piece
+      #   add the location + dr/dc to moves
+    end
+    moves
+  end
 end
