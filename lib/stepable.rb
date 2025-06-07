8b25ea4679391e3dc6e249e72f12d10bafd30abf
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-06-07 18:45:14 +0500

    loop removed

--- lib/stepable.rb
+++ lib/stepable.rb
@@ -5,19 +5,17 @@ module Stepable
     move_dirs.each do |(dr, dc)|
       current_r, current_c = location
 
-      loop do
-        current_r += dr
-        current_c += dc
-        loc = [current_r, current_c]
-        break if !board.in_bounds?(loc)
+      current_r += dr
+      current_c += dc
+      loc = [current_r, current_c]
+      break if !board.in_bounds?(loc)
 
-        if board.empty?(loc)
-          moves << loc
-        end
-        if enemy?(loc)
-          moves << loc
-          break
-        end
+      if board.empty?(loc)
+        moves << loc
+      end
+      if enemy?(loc)
+        moves << loc
+        break
       end
     end
     moves
