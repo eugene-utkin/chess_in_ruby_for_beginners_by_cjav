4e688bddb66a3e3328065714fb42ef0982fc9585
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-06-10 16:41:14 +0500

    king created

--- main.rb
+++ main.rb
@@ -3,8 +3,8 @@ require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
 
 b = Board.new
-b[[0, 0]] = Rook.new(b, [0, 0], :black)
-p b[[0, 0]].available_moves
+b[[2, 2]] = King.new(b, [2, 2], :black)
+p b[[2, 2]].available_moves
 
 # text_board = BoardRendererText.new(b)
 # puts text_board.render
