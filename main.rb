66658171fed1612fb02d7ec4396dd1df83f9c491
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-06-17 22:49:44 +0500

    old code commented out

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ require_relative './lib/pieces.rb'
 require_relative './lib/board.rb'
 require_relative './lib/board_renderer_text.rb'
 
-b = Board.new
-b[[0, 0]] = King.new(b, [0, 0], :black)
-p b[[0, 0]].available_moves
+# b = Board.new
+# b[[0, 0]] = King.new(b, [0, 0], :black)
+# p b[[0, 0]].available_moves
 
 # text_board = BoardRendererText.new(b)
 # puts text_board.render
