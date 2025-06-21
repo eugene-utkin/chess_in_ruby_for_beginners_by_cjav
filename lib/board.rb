ea7182d07d7c54fe6d753760e8807ec387495674
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-06-21 22:49:00 +0500

    added arguments for piece creation

--- lib/board.rb
+++ lib/board.rb
@@ -18,7 +18,7 @@ class Board
     [[0, :black], [7, :white]].each do |(r, color)|
       board[[r, 0]] = Rook.new(self, [r, 0], color)
       board[[r, 7]] = Rook.new(self, [r, 7], color)
-      board[[r, 1]] = Knight.new(color)
+      board[[r, 1]] = Knight.new(self, [r, 1], color)
       board[[r, 6]] = Knight.new(color)
       board[[r, 2]] = Bishop.new(color)
       board[[r, 5]] = Bishop.new(color)
