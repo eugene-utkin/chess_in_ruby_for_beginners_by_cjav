65d836c6d8988ddbbc11d94a8c8a3a2d4c820a42
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-07-10 22:17:02 +0500

    one more self changed to board

--- lib/board.rb
+++ lib/board.rb
@@ -16,7 +16,7 @@ class Board
     end
 
     [[0, :black], [7, :white]].each do |(r, color)|
-      board[[r, 0]] = Rook.new(self, [r, 0], color)
+      board[[r, 0]] = Rook.new(board, [r, 0], color)
       board[[r, 7]] = Rook.new(self, [r, 7], color)
       board[[r, 1]] = Knight.new(self, [r, 1], color)
       board[[r, 6]] = Knight.new(self, [r, 6], color)
