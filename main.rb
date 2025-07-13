1152337838f63886f805988b21ff1a28c5033814
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-07-13 21:27:57 +0500

    puts replaced with p

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ require_relative './lib/board_renderer_text.rb'
 b = Board.start_chess
 text_board = BoardRendererText.new(b)
 puts text_board.render
-puts b[[1, 1]].available_moves
+p b[[1, 1]].available_moves
 
 # pawn = Pawn.new([0, 0], :black)
 # p pawn.to_s
