04248e6d983b856b1cced1887bda3b48b35ce682
Evgeniy Utkin <eugene.utkin@gmail.com>
2025-08-06 21:29:45 +0500

    swap_player! method created

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,10 @@ class Game
     @current_player = @player1
   end
 
+  def swap_player!
+
+  end
+
   def play
     # Prompt current player to enter a starting pos
     # Prompt current player to enter an ending pos
